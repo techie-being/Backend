1d02181656ada8a981c031310e7f99b1b4dc4f77
rakesh <014rakeshofficial@gmail.com>
2026-01-06 11:46:37 +0530

    githubdata URL added

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get('/twitter',(req,res)=>{
 
 })
 
+const githubdata={
+  
+  
+  "email": null,
+  "hireable": null,
+  "bio": " Tech enthusiast passionate about DSA, web dev (Python, Flask, JS). Sharpening problem-solving skills and building real-world projects.",
+  "twitter_username": null,
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2025-06-13T20:57:04Z",
+  "updated_at": "2025-10-01T16:05:33Z"
+}
+
+app.get('/github',(req,res)=>{
+  res.json(githubdata)
+})
 app.get('/login',(req,res)=>{
     res.send('login to see any thing')
 
